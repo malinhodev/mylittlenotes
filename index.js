80868b514b91c072bc1896f74da3271259370f5a
malinhodev <mcterasgo@gmail.com>
2023-11-02 20:34:48 -0300

    html e css finalizados conexão com o mongo estabelacida

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const bodyParser = require('body-parser')
 const app =  express()
 const port = 8000
 
+const db = require('./db/connection')
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const notesRoutes = require('./routes/notes')
+
+app.use('/notes', notesRoutes)
+
 app.get('/', function(req,res){
-    res.send('O aplicativo está funcionando')
+    res.render('home')
 })
 
-app.listen(port,()=>{
-    console.log(`Projeto rodando porta: ${port}`)
+db.initDb((err,db)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('O Mongo conectou!!')
+        app.listen(port, ()=>{
+            console.log(`rodando na porta: ${port}`)
+        })
+    }
 })
